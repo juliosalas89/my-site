95985814e4f041695d7e3e5f54e57ff14a8aa52d
julio <julio.a.salas89@gmail.com>
2023-08-20 16:46:23 +0200

    some minor design changes, and all the texts were reviewed

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -4,7 +4,7 @@ import { urbanist600, urbanist100 } from "@/utils/fonts"
 import { forwardRef, useRef } from "react"
 import EducationCard from "./EducationCard"
 import AnimatedAppearance from "../general/AnimatedAppearance"
-import educ from '../../appData/education.json'
+import educ from '../../appContent/education.json'
 
 const Education = forwardRef(function Education (_,ref) {
     const educationCards = useRef(educ.list)
@@ -18,8 +18,7 @@ const Education = forwardRef(function Education (_,ref) {
                             <p className="title-mid"><span className="gradient-text">and</span> degrees</p>
                         </div>
                         <div className={`subtitle-div title-right ${urbanist100.className}`}>
-                            <p>I know reading a lot of cards just to know how much i know is annoying, so to be short:</p>
-                            <p>I&#039;m have a degree in Mechanical Engineering and I know some JavaScript Stacks.</p>
+                            <p>I got a degree in Mechanical Engineering (I love science in general) and years later I found a passion in Software development.</p>
                         </div>
                     </div>
                 </AnimatedAppearance>
