a09fcac987b5c95d8426ef27681723a915372337
julio <julio.a.salas89@gmail.com>
2023-09-09 17:54:49 +0200

    privacy policy created to be used in applications with ads

---
+++ src/app/privacy-policy-es/page.js
@@ -0,0 +1,37 @@
+
+export default function PrivacyPolicyEs () {
+    return (
+        <main className="privacy-policy-main">
+            <h3 className="privacy-policy-title">Política de Privacidad de Units Converter JS</h3>
+            <div className="privacy-policy-content">
+                <p>Fecha de entrada en vigor: Septiembre 1, 2023</p>
+                <p>Esta Política de Privacidad describe cómo Units Converter JS ("nosotros", "nuestra" o "la Aplicación") recopila y utiliza información cuando los usuarios utilizan nuestra aplicación móvil. Respetamos la privacidad de nuestros usuarios y estamos comprometidos en proteger su información personal. Esta política se aplica a la información recopilada a través de nuestra aplicación.</p>
+            </div>
+            <h3 className="privacy-policy-subtitle">Recopilación y Uso de Datos</h3>
+            <ul className="privacy-policy-content">
+                <li>
+                    <p><span className="privacy-policy-span">1- Información Personal:</span>  La aplicación Units Converter JS no recopila ni solicita de manera activa ningún tipo de información personal identificable de los usuarios, como nombres, direcciones de correo electrónico o números de teléfono.</p>
+                </li>
+                <li>
+                    <p><span className="privacy-policy-span">2- Publicidad:</span> Utilizamos servicios de publicidad proporcionados por Google AdMob para mostrar anuncios en nuestra aplicación. Google AdMob puede recopilar el Identificador de Publicidad (ID de publicidad) de los usuarios de la aplicación con el fin de ofrecer anuncios personalizados o no personalizados. Para obtener más información sobre cómo Google AdMob utiliza los datos, consulta la política de privacidad de Google.</p>
+                </li>
+            </ul>
+            <h3 className="privacy-policy-subtitle">Cookies y Tecnologías Similares</h3>
+            <div className="privacy-policy-content">
+                <p>Nuestra aplicación no utiliza cookies ni tecnologías similares para recopilar información de los usuarios.</p>
+            </div>
+            <h3 className="privacy-policy-subtitle">Cambios en la Política de Privacidad</h3>
+            <div className="privacy-policy-content">
+                <p>Podemos actualizar esta Política de Privacidad en el futuro para reflejar cambios en nuestras prácticas de recopilación y uso de datos o cambios en las regulaciones de privacidad aplicables. Cualquier cambio importante en esta política se notificará a los usuarios a través de una actualización de la aplicación.</p>
+            </div>
+            <h3 className="privacy-policy-subtitle">Contacto</h3>
+            <div className="privacy-policy-content">
+                <p>Si tienes preguntas o inquietudes sobre nuestra Política de Privacidad, por favor contáctanos en julio.a.salas89@gmail.com, estaremos encantados de responder a tus preguntas.</p>
+            </div>
+            <h3 className="privacy-policy-subtitle">Administrar Consentimiento</h3>
+            <div className="privacy-policy-content">
+                <p>Si quieres cambiar el conseintieminto puedes hacerlo en cualquier momento en las configuraciones de la applicacion.</p>
+            </div>
+        </main>
+    )
+} 
---
+++ src/app/privacy-policy-ge/page.js
@@ -0,0 +1,37 @@
+
+export default function PrivacyPolicyGe () {
+    return (
+        <main className="privacy-policy-main">
+            <h3 className="privacy-policy-title">Datenschutzrichtlinie für Converter JS</h3>
+            <div className="privacy-policy-content">
+                <p>Gültig ab: September 1, 2023</p>
+                <p>Diese Datenschutzrichtlinie beschreibt, wie Units Converter JS ("wir," "uns" oder "die App") Informationen sammelt und verwendet, wenn Benutzer unsere mobile Anwendung nutzen. Wir respektieren die Privatsphäre unserer Benutzer und sind verpflichtet, ihre persönlichen Informationen zu schützen. Diese Richtlinie gilt für Informationen, die über unsere Anwendung gesammelt werden.</p>
+            </div>
+            <h3 className="privacy-policy-subtitle">Datensammlung und -verwendung</h3>
+            <ul className="privacy-policy-content">
+                <li>
+                    <p><span className="privacy-policy-span">1- Persönliche Informationen:</span>  Die Anwendung Units Converter JS sammelt oder fordert nicht aktiv personenbezogene Informationen von Benutzern an, wie Namen, E-Mail-Adressen oder Telefonnummern.</p>
+                </li>
+                <li>
+                    <p><span className="privacy-policy-span">2- Werbung:</span> Wir verwenden Werbedienste, die von Google AdMob bereitgestellt werden, um Anzeigen in unserer Anwendung anzuzeigen. Google AdMob kann die Werbe-ID (Advertising Identifier - IDFA) von Benutzern der Anwendung sammeln, um personalisierte oder nicht personalisierte Anzeigen bereitzustellen. Weitere Informationen zur Verwendung von Daten durch Google AdMob finden Sie in der Datenschutzrichtlinie von Google.</p>
+                </li>
+            </ul>
+            <h3 className="privacy-policy-subtitle">Cookies und ähnliche Technologien</h3>
+            <div className="privacy-policy-content">
+                <p>Unsere Anwendung verwendet keine Cookies oder ähnlichen Technologien, um Informationen von Benutzern zu sammeln.</p>
+            </div>
+            <h3 className="privacy-policy-subtitle">Änderungen an der Datenschutzrichtlinie</h3>
+            <div className="privacy-policy-content">
+                <p>Wir können diese Datenschutzrichtlinie in der Zukunft aktualisieren, um Änderungen in unseren Datensammlungs- und -nutzungspraktiken oder Änderungen in geltenden Datenschutzbestimmungen widerzuspiegeln. Wesentliche Änderungen an dieser Richtlinie werden den Benutzern über ein App-Update mitgeteilt.</p>
+            </div>
+            <h3 className="privacy-policy-subtitle">Kontakt</h3>
+            <div className="privacy-policy-content">
+                <p>Wenn Sie Fragen oder Bedenken zu unserer Datenschutzrichtlinie haben, kontaktieren Sie uns bitte unter julio.a.salas89@gmail.com, wir stehen Ihnen gerne zur Verfügung, um Ihre Anfragen zu beantworten.</p>
+            </div>
+            <h3 className="privacy-policy-subtitle">Einwilligungsverwaltung</h3>
+            <div className="privacy-policy-content">
+                <p>Wenn Sie Ihre Einwilligung ändern möchten, können Sie dies jederzeit in den Anwendungseinstellungen tun.</p>
+            </div>
+        </main>
+    )
+} 
